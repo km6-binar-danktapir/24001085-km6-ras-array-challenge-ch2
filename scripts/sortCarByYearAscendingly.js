be2e052adb54fafd5b2d9936b9ffb281d3a9dde6
Rasyad Jabbar Poncosewoyo <rasyadponcosewoyo@gmail.com>
2024-03-09 22:28:46 +0700

    removed .gitignore

---
+++ scripts/sortCarByYearAscendingly.js
@@ -0,0 +1,20 @@
+function sortCarByYearAscendingly(cars) {
+    // Sangat dianjurkan untuk console.log semua hal hehe
+    console.log(cars);
+
+    // Clone array untuk menghindari side-effect
+    // Apa itu side effect?
+    const result = [...cars];
+
+    // Tulis code-mu disini
+    result.sort((carA, carB) => {
+      if (carA.year < carB.year) {
+        return -1;
+      } else {
+        return 1;
+      }
+    });
+
+    // Rubah code ini dengan array hasil sorting secara ascending
+    return result;
+}
